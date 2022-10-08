f220981fe38bf1acb84daa20f9cfea391c90e995
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-10-08 13:07:26 +0530

    trending coins carousel done

---
+++ src/components/Banner.js
@@ -0,0 +1,60 @@
+import { Container, makeStyles, Typography } from '@material-ui/core'
+import React from 'react'
+import Carousel from './Carousel';
+
+const useStyles = makeStyles(() => ({
+    banner: {
+        backgroundImage: 'url(./banner2.jpg)'
+    },
+    bannerContent: {
+        height: 400,
+        display: 'flex',
+        flexDirection: 'column',
+        paddingTop: 25,
+        justifyContent: 'space-around'
+    },
+    tagLine: {
+        display: 'flex',
+        height: '40%',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        textAlign: 'center'
+    }
+}))
+
+const Banner = () => {
+    const classes = useStyles();
+    return (
+    <div className={classes.banner}>
+        <Container className={classes.bannerContent}>
+            <div className={classes.tagLine}>
+                <Typography
+                    variant='h2'
+                    style={{
+                        fontWeight: 'bold',
+                        marginBottom: 15,
+                        fontFamily: 'Montserrat'
+                    }}
+                >
+                    Crypto Predator
+                </Typography>
+
+                <Typography
+                    variant='subtitle2'
+                    style={{
+                        color: 'darkgrey',
+                        textTransfort: 'capitalize',
+                        fontFamily: 'Montserrat'
+                    }}
+                >
+                    Get all the info regarding your favorite Crypto Currency
+                </Typography>
+            </div>
+            
+            <Carousel />
+        </Container>
+    </div>
+  )
+}
+
+export default Banner
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom';
+import "react-alice-carousel/lib/alice-carousel.css";
 import CryptoContext from './CryptoContext';
+
+// 52:25
 
 ReactDOM.render(
   <React.StrictMode>
