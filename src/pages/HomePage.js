f954119b5f13187c5e54c717b7acafc5e83e1c51
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-10-10 21:45:02 +0530

    coinsTable, pagination done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import CoinsTable from '../components/CoinsTable'
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <CoinsTable />
     </>
   )
 }
