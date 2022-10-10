8c754c26b6abee2a16f1ea093830b02bb946d394
Aditya Kanikdaley <adikanikdaley@gmail.com>
2022-10-10 21:46:21 +0530

    eslint-disable-next-line, page-formatting done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,19 +7,19 @@ import AliceCarousel from 'react-alice-carousel';
 import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-    carousel: {
-      height: '50%',
-      display: 'flex',
-      alignItems: 'center'
-    },
-    carouselItem: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      cursor: 'pointer',
-      textTransform: 'uppercase',
-      color: 'white'
-    }
+  carousel: {
+    height: '50%',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  carouselItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+    color: 'white'
+  }
 }));
 
 export function numberWithCommas(x) {
@@ -31,7 +31,7 @@ const Carousal = () => {
   const [trending, setTrending] = useState([]);
   const classes = useStyles();
 
-  const { currency , symbol} = CryptoState();
+  const { currency, symbol } = CryptoState();
 
   const fetchTrendingCoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
@@ -42,38 +42,39 @@ const Carousal = () => {
 
   useEffect(() => {
     fetchTrendingCoins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currency]);
-  
+
   const items = trending.map((coin) => {
     let profit = coin.price_change_percentage_24h >= 0;
-      return (
-        <Link 
-          className={classes.carouselItem} 
-          to={`/coins/${coin.id}`} 
-        >
-          <img 
-            src={coin?.image}
-            alt={coin.name}
-            height='80'
-            style={{ marginBottom: 10}}
-          />
-          <span>{coin?.symbol}
-            &nbsp;
-            <span
-              style={{
-                color: profit > 0 ? 'rgb(14, 203, 129)' : 'red',
-                fontWeight: 500
-              }}
-            >
-              {profit && '+'} {coin?.price_change_percentage_24h?.toFixed(2)}%
-            </span>
-          </span> 
-
-          <span style={{fontSize: 22, fontWeight: 500}}>
-            {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+    return (
+      <Link
+        className={classes.carouselItem}
+        to={`/coins/${coin.id}`}
+      >
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height='80'
+          style={{ marginBottom: 10 }}
+        />
+        <span>{coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? 'rgb(14, 203, 129)' : 'red',
+              fontWeight: 500
+            }}
+          >
+            {profit && '+'} {coin?.price_change_percentage_24h?.toFixed(2)}%
           </span>
-          </Link>
-      );
+        </span>
+
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
   });
 
   const responsive = {
@@ -97,7 +98,7 @@ const Carousal = () => {
         responsive={responsive}
         autoPlay
         items={items}
-        />
+      />
     </div>
   )
 }
