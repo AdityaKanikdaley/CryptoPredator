d806fc11851c50416f21d70cfce59280b6291a68
AdityaKanikdaley <adikanikdaley@gmail.com>
2022-11-02 08:38:35 +0530

    select btn neumorphic

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -3,19 +3,23 @@ import { makeStyles } from "@material-ui/core";
 const SelectButton = ({ children, selected, onClick }) => {
   const useStyles = makeStyles({
     selectbutton: {
-      border: "2px solid black",
-      borderRadius: 5,
+      // border: "2px solid black",
+      borderRadius: "10px",
       padding: "10px 5px 10px 5px", // top-right-bottom-left
       fontFamily: "Montserrat",
       cursor: "pointer",
+
       // backgroundColor: selected ? "" : "",
-      color: "black",
+      color: "#606060",
+      boxShadow: "-10px -10px 15px rgba(255, 255, 255, 0.5), 10px 10px 15px rgb(70, 70, 70, 0.12)",
+      backgroundColor: "#ececec",
       fontWeight: selected ? 700 : 500,
       "&:hover": {
         backgroundColor: "#505050",
         color: "#ececec",
       },
-      width: "24%",
+      width: "20%",
+      textAlign: "center",
     },
   });
 
